ed46e1ebf25e2759ce1675cf3f56c834c503d61a
Romain Texier <texier.r@gmail.com>
2022-01-25 01:29:51 +0100

    initial commit

---
+++ src/app.js
@@ -0,0 +1,16 @@
+import express from 'express';
+import path from 'path';
+
+const app = express();
+
+app.set('views', path.resolve('views'));
+
+app.set('view engine', 'pug');
+
+app.get('/', (req, res) => {
+    res.render('home');
+});
+
+app.use(express.static(path.resolve('public')));
+
+export default app; 
