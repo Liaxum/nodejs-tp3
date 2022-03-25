b5dedbf244c3675a9cb9888f3fe66a60ee8c906c
Romain <texier.r@gmail.com>
2022-03-25 14:46:17 +0100

    @upd

--- front/src/router/index.js
+++ front/src/router/index.js
@@ -1,31 +1,67 @@
-import Vue from 'vue'
-import VueRouter from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import Vue from "vue";
+import VueRouter from "vue-router";
+import store from "../store";
+import Home from "../views/Home.vue";
+import Register from "../views/Register";
+import Login from "../views/Login";
+import Posts from "../views/Posts";
 
-Vue.use(VueRouter)
+Vue.use(VueRouter);
 
 const routes = [
   {
-    path: '/',
-    name: 'home',
-    component: HomeView
+    path: "/",
+    name: "Home",
+    component: Home,
   },
   {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: function () {
-      return import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
-    }
-  }
-]
+    path: "/register",
+    name: "Register",
+    component: Register,
+    meta: { guest: true },
+  },
+  {
+    path: "/login",
+    name: "Login",
+    component: Login,
+    meta: { guest: true },
+  },
+  {
+    path: "/posts",
+    name: "Posts",
+    component: Posts,
+    meta: { requiresAuth: true },
+  },
+];
 
 const router = new VueRouter({
-  mode: 'history',
+  mode: "history",
   base: process.env.BASE_URL,
-  routes
-})
+  routes,
+});
 
-export default router
+router.beforeEach((to, from, next) => {
+  if (to.matched.some((record) => record.meta.requiresAuth)) {
+    if (store.getters.isAuthenticated) {
+      next();
+      return;
+    }
+    next("/login");
+  } else {
+    next();
+  }
+});
+
+router.beforeEach((to, from, next) => {
+  if (to.matched.some((record) => record.meta.guest)) {
+    if (store.getters.isAuthenticated) {
+      next("/posts");
+      return;
+    }
+    next();
+  } else {
+    next();
+  }
+});
+
+export default router;
